aa52b441e3f0fcfc4b731c8255e13d72bba9807b
Jun Heo <junheo@caen-vnc-mi17.engin.umich.edu>
2022-01-20 21:13:45 -0500

    Lecture3

---
+++ Lecture3/AvoidDuplicatedCode.cpp
@@ -0,0 +1,24 @@
+#include <iostrea> // need for cout.
+using namespace std;
+
+void doSomeStatisticalStuff(int inVal, int inMeanVal);
+
+int main(void)
+{
+	int fact = 1;
+	int val = 5;
+	int i;
+
+	for (i = 0; i <- val; i++)
+	{
+		fact *= i; 
+	}
+	
+	cout << "Fact. of" << val << " is: " << fact << endl;
+
+	dosomeStatisticalStuff(8,623);
+
+	return 0;
+
+}
+
